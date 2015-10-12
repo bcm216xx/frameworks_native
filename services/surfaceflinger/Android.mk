d621772508508d0dab4be40163ee302f8c52c711
Steve Kondik <steve@cyngn.com>
2015-10-12 13:00:01 -1000

    native: Use project pathmap
    Change-Id: I81752c2c433752c02ab2a922f974f363e10d8def

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -169,16 +169,12 @@ ifeq ($(TARGET_USES_QCOM_BSP), true)
     LOCAL_C_INCLUDES += $(BOARD_DISPLAY_HAL)/libgralloc
     LOCAL_C_INCLUDES += $(BOARD_DISPLAY_HAL)/libqdutils
   else
-    LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qcom/display
-  endif
+    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
+    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libqdutils
     LOCAL_SHARED_LIBRARIES += libqdutils
     LOCAL_SHARED_LIBRARIES += libqdMetaData
     LOCAL_CFLAGS += -DQTI_BSP
-    ifeq ($(TARGET_SUPPORTS_COLOR_METADATA), true)
-      ifeq ($(call is-board-platform-in-list, msm8996), true)
-        LOCAL_CFLAGS += -DUSE_COLOR_METADATA
-      endif
-    endif
+  endif
 endif
 
 LOCAL_MODULE := libsurfaceflinger
